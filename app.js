4e9e3c007d437864ac8b0624ae871e623de226bb
shubhamdixit863 <shubhamdixit863@gmail.com>
2022-06-23 13:39:53 +0530

    added some test cases

--- app.js
+++ app.js
@@ -2,5 +2,26 @@ function sum(a, b) {
     return a + b;
   }
 
+
+  function loweCaseString(str){
+
+    return str.toLowerCase();
+
+  }
+
+  function upperCaseString(str){
+
+    return str.toUpperCase();
+
+  }
+
+  function testInArray(arra){
+
+    return arra.map(ele=>ele.toLowerCase());
+
+  }
+
+  /// expect(new Set(shoppingList)).toContain('milk');
+
   //console.log(sum(2,3));
-  module.exports = sum;
+  module.exports = sum
